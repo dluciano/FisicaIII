66062cf794555a3f015ea281a058f7c4361c2b4e
Alvin Durán <nicoavn@gmail.com>
2015-10-15 13:00:24 -0400

    Stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function Magnitud(valor, unidad){
+function Magnitud(valor, unidad) {
     var self = this;
     self.valor = valor;
     self.unidad = unidad;
@@ -12,7 +12,7 @@ var Dimensiones = {
     intensidadCorriente: "A"
 };
 
-function Unidad(nombre, dimension, simbolo){
+function Unidad(nombre, dimension, simbolo) {
     var self = this;
     self.nombre = nombre;
     self.dimension = dimension;
@@ -21,7 +21,7 @@ function Unidad(nombre, dimension, simbolo){
     return self;
 }
 
-function UnidadDerivada(nombre, unidadesNumerador, unidadesDenominador, simbolo){
+function UnidadDerivada(nombre, unidadesNumerador, unidadesDenominador, simbolo) {
     var self = this;
     self.nombre = nombre;
     self.unidadesNumerador = unidadesNumerador;
@@ -31,7 +31,7 @@ function UnidadDerivada(nombre, unidadesNumerador, unidadesDenominador, simbolo)
     return self;
 }
 
-function Prefijo(nombre, simbolo, exponente, base){
+function Prefijo(nombre, simbolo, exponente, base) {
     var self = this;
     self.nombre = nombre;
     self.simbolo = simbolo;
@@ -40,64 +40,67 @@ function Prefijo(nombre, simbolo, exponente, base){
     self.base = base === undefined ? 10 : base;
 }
 
-function SistemaMedida(nombre){
+function SistemaMedida(nombre) {
     var self = this;
     self.nombre = nombre;
     self.unidades = [];
     self.unidadesDerivadas = [];
     self.prefijos = [];
-    
-    self.agregarUnidad = function(unidad){
+
+    self.agregarUnidad = function (unidad) {
         unidad.sistema = self;
         self.unidades.push(unidad);
         return self;
     };
-    
-    self.agregarUnidadDerivada = function(unidad){
+
+    self.agregarUnidadDerivada = function (unidad) {
         unidad.sistema = self;
         self.unidadesDerivadas.push(unidad);
         return self;
     };
-    
-    self.agregarPrefijo = function(prefijo){
+
+    self.agregarPrefijo = function (prefijo) {
         prefijo.sistema = self;
         self.prefijos.push(prefijo);
         return self;
     };
-    
+
     return self;
 }
 
-function RepositorioUnidades(){
+function RepositorioUnidades() {
     var self = this;
-    
+
     self.sistemas = [];
-    
+
     var amperio = new Unidad("amperio", Dimensiones.intensidadCorriente, "A");
     var segundo = new Unidad("segundo", Dimensiones.tiempo, "s");
     var si = new SistemaMedida("internacional")
-            .agregarUnidad(new Unidad("metro", Dimensiones.longitud, "m"))
-            .agregarUnidad(new Unidad("kilogramo", Dimensiones.masa, "Kg"))
-            .agregarUnidad(segundo)
-            .agregarUnidad(amperio);
-    
-    self.sistemas.push(si);
-    
-    si.agregarUnidadDerivada(new UnidadDerivada("culombio", [amperio, segundo],null,"C"));
+        .agregarUnidad(new Unidad("metro", Dimensiones.longitud, "m"))
+        .agregarUnidad(new Unidad("kilogramo", Dimensiones.masa, "Kg"))
+        .agregarUnidad(segundo)
+        .agregarUnidad(amperio);
 
-    si.agregarPrefijo(new Prefijo("deca", "da", 1));
-    si.agregarPrefijo(new Prefijo("hecto", "h", 2));
-    si.agregarPrefijo(new Prefijo("kilo", "k", 3));
+    self.sistemas.push(si);
+
+    si.agregarUnidadDerivada(new UnidadDerivada("culombio", [amperio, segundo], null, "C"));
+
+    si.agregarPrefijo(new Prefijo("yotta", "Y", 24));
+    si.agregarPrefijo(new Prefijo("zetta", "Z", 21));
+    si.agregarPrefijo(new Prefijo("exa", "E", 18));
+
+    si.agregarPrefijo(new Prefijo("peta", "P", 15));
+    si.agregarPrefijo(new Prefijo("tera", "T", 12));
+    si.agregarPrefijo(new Prefijo("giga", "G", 9));
     si.agregarPrefijo(new Prefijo("mega", "M", 6));
-    si.agregarPrefijo(new Prefijo("giga","G", 9));
-    si.agregarPrefijo(new Prefijo("tera","T", 12));
-    si.agregarPrefijo(new Prefijo("peta","P", 15));
-    si.agregarPrefijo(new Prefijo("exa","E", 18));
-    si.agregarPrefijo(new Prefijo("zetta","Z", 21));
-    si.agregarPrefijo(new Prefijo("yotta","Y", 24));
-        
-    si.agregarPrefijo(new Prefijo("unidad","", 0));
-    
+
+    si.agregarPrefijo(new Prefijo("kilo", "k", 3));
+    si.agregarPrefijo(new Prefijo("hecto", "h", 2));
+    si.agregarPrefijo(new Prefijo("deca", "da", 1));
+
+
+    si.agregarPrefijo(new Prefijo("unidad", "", 0));
+
     si.agregarPrefijo(new Prefijo("deci", "d", -1));
     si.agregarPrefijo(new Prefijo("centi", "c", -2));
     si.agregarPrefijo(new Prefijo("mili", "m", -3));
@@ -106,51 +109,51 @@ function RepositorioUnidades(){
     si.agregarPrefijo(new Prefijo("pico", "p", -12));
     si.agregarPrefijo(new Prefijo("femto", "f", -15));
     si.agregarPrefijo(new Prefijo("atto", "a", -18));
-    
+
     return self;
 }
 
-function UnidadesVM(unidad, prefijos){
+function UnidadesVM(unidad, prefijos) {
     var self = this;
     self.unidad = unidad;
     self.opciones = [];
-    $(prefijos).each(function(i, p){
+    $(prefijos).each(function (i, p) {
         self.opciones.push(new Option(p.simbolo + self.unidad.simbolo, p.exponente, p.exponente === 0));
     });
     return self;
 }
 
-function UnidadComponente(si, element){
+function UnidadComponente(si, element) {
     var self = this;
     self.element = element;
     var vm = {};
-    //SHIT...
+    //Holy SHIT...
     try {
-        vm = new UnidadesVM(Enumerable.From(si.unidades).Single(function(unid){
+        vm = new UnidadesVM(Enumerable.From(si.unidades).Single(function (unid) {
             return unid.nombre == $(self.element).data("unidad");
         }), si.prefijos);
     } catch (e) {
         try {
-            vm = new UnidadesVM(Enumerable.From(si.unidadesDerivadas).Single(function(unid){
+            vm = new UnidadesVM(Enumerable.From(si.unidadesDerivadas).Single(function (unid) {
                 return unid.nombre == $(self.element).data("unidad");
             }), si.prefijos);
         } catch (e) {
             throw e;
         }
     }//----SHITT...
-    $(vm.opciones).each(function(i, o){
+    $(vm.opciones).each(function (i, o) {
         $(self.element).append($(o));
     });
     return self;
 }
 
-function Initialize(){
+function Initialize() {
     var self = this;
     self.si = new RepositorioUnidades().sistemas[0];
-    
-    $("*").filter(function() {
+
+    $("*").filter(function () {
         return $(this).data("unidad") !== undefined;
-    }).each(function(e,sl){
+    }).each(function (e, sl) {
         UnidadComponente(self.si, sl);
-    });    
+    });
 }
